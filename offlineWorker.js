fa35f3e17c2a0faa81c038ac5a6507f779fa2864
Patrick Winchell <pat@tekgo.org>
2020-04-27 20:56:45 -0700

    Add sound files to offline worker

--- offlineWorker.js
+++ offlineWorker.js
@@ -8,10 +8,48 @@ const fileList = [
 	'/OkayArtist/imageDisplay.html',
 	'/OkayArtist/index.html',
 	'/OkayArtist/offlineSupport.js',
-	'/OkayArtist/'
+	'/OkayArtist/',
+	'OkayArtist/audio/brush_1.mp3',
+	'OkayArtist/audio/brush_2.mp3',
+	'OkayArtist/audio/brush_3.mp3',
+	'OkayArtist/audio/brush_4.mp3',
+	'OkayArtist/audio/brush_5.mp3',
+	'OkayArtist/audio/brush_6.mp3',
+	'OkayArtist/audio/brush_7.mp3',
+	'OkayArtist/audio/brush_8.mp3',
+	'OkayArtist/audio/brush_9.mp3',
+	'OkayArtist/audio/devon-bg1.mp3',
+	'OkayArtist/audio/devon-stairwell.mp3',
+	'OkayArtist/audio/loop_0.mp3',
+	'OkayArtist/audio/magic1.mp3',
+	'OkayArtist/audio/numkey_0.mp3',
+	'OkayArtist/audio/numkey_1.mp3',
+	'OkayArtist/audio/numkey_10.mp3',
+	'OkayArtist/audio/numkey_11.mp3',
+	'OkayArtist/audio/numkey_12.mp3',
+	'OkayArtist/audio/numkey_2.mp3',
+	'OkayArtist/audio/numkey_3.mp3',
+	'OkayArtist/audio/numkey_4.mp3',
+	'OkayArtist/audio/numkey_5.mp3',
+	'OkayArtist/audio/numkey_6.mp3',
+	'OkayArtist/audio/numkey_7.mp3',
+	'OkayArtist/audio/numkey_8.mp3',
+	'OkayArtist/audio/numkey_9.mp3',
+	'OkayArtist/audio/sfx_0.mp3',
+	'OkayArtist/audio/sfx_0_anti.mp3',
+	'OkayArtist/audio/sfx_1.mp3',
+	'OkayArtist/audio/sfx_2.mp3',
+	'OkayArtist/audio/sfx_3.mp3',
+	'OkayArtist/audio/sfx_4.mp3',
+	'OkayArtist/audio/sfx_5.mp3',
+	'OkayArtist/audio/sfx_6.mp3',
+	'OkayArtist/audio/sfx_7.mp3',
+	'OkayArtist/audio/sfx_8.mp3',
+	'OkayArtist/audio/streetnoise.mp3',
+	'OkayArtist/audio/time_up.mp3'
 ];
 
-const version = 'x4';
+const version = 'x5';
 
 self.addEventListener('install', function(event) {
 	event.waitUntil(
